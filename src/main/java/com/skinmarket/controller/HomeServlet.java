836971521ad40c618089865e7886f4eeae9838ca
roboyess2 <roboyess2@yandex.ru>
2026-05-21 23:26:28 +0300

    some bug fixes

--- src/main/java/com/skinmarket/controller/HomeServlet.java
+++ src/main/java/com/skinmarket/controller/HomeServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet({"/", "/home"})
+@WebServlet({"/home"})
 public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
