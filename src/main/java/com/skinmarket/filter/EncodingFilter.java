aa0c343e39bfec5a85e321aa25111def5722afe6
roboyess2 <roboyess2@yandex.ru>
2026-05-22 17:52:45 +0300

    tomcat encoding fixes 2

---
+++ src/main/java/com/skinmarket/filter/EncodingFilter.java
@@ -0,0 +1,24 @@
+package com.skinmarket.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class EncodingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+    @Override
+    public void destroy() {}
+}
